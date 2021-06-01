3069e626f6eeda4a930b5052c8371b6e53286fd2
LeoneM3 <73596072+LeoneM3@users.noreply.github.com>
2021-06-01 14:49:33 -0600

    primera build
    v0.0.1

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -5,40 +5,56 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    Rigidbody rb;
-    Vector2 inputMov;
-    public float velCamina = 10f;
-    public float velCorre = 20f;
+    public Camera FPSCamara;
+     
+    public float horizontalSpeed;
+    public float verticalSpeed;
 
- 
+    float h;
+    float v;
+
+
+
     void Start()
     {
-
-      rb = GetComponent<Rigidbody>();
-
-
-    }
-
-    
-    void Update()
-    {
-
-        inputMov.x = Input.GetAxis("Horizontal");
-        inputMov.y = Input.GetAxis("Vertical");
         
     }
-
-
-
-    private void FixedUpdate()
-    {
-      
-      float vel = Input.GetKey(KeyCode.LeftShift) ? velCorre : velCamina;
-
-      rb.velocity = transform.forward * vel * inputMov.y + transform.right * vel * inputMov.x;
-      
-      
-    }
-
     
+
+  void Update() {
+
+    h = horizontalSpeed * Input.GetAxis ("Mouse X");
+    v = verticalSpeed * Input.GetAxis ("Mouse Y");
+
+    transform.Rotate (0, h, 0);
+    FPSCamara.transform.Rotate (-v, 0 ,0 );
+
+
+    if ( Input.GetKey (KeyCode.W)) {
+
+      transform.Translate(0, 0 , 0.1f);
+   
+    } else {
+
+      if (Input.GetKey (KeyCode.S)){
+        transform.Translate (0, 0, -0.1f);
+      } else {
+
+        if (Input.GetKey (KeyCode.A)){
+        transform.Translate (-0.1f, 0, 0);
+      } else {
+        if (Input.GetKey (KeyCode.D)){
+        transform.Translate (0.1f, 0, 0);
+      } 
+      } 
+
+      }
+
+    }
+    
+
+
+
+
+   }
 }
