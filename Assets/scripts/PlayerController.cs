ec2b56f26647d57af27b6da65cfdd35d9f740b99
LeoneM3 <73596072+LeoneM3@users.noreply.github.com>
2021-06-04 04:27:27 -0600

    mejoras de velocidad y nuevos modelos

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,7 +6,13 @@ public class PlayerController : MonoBehaviour
 {
 
     public Camera FPSCamara;
-     
+    Rigidbody rb;
+    Vector2 inputMov;
+
+    public float velCamina = 10f;
+    public float velCorre = 20f;
+
+
     public float horizontalSpeed;
     public float verticalSpeed;
 
@@ -17,44 +23,42 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
     
 
   void Update() {
 
-    h = horizontalSpeed * Input.GetAxis ("Mouse X");
+
+
+    //rotacion del mouse//
+    h = horizontalSpeed * Input.GetAxis ("Mouse X"); 
     v = verticalSpeed * Input.GetAxis ("Mouse Y");
 
     transform.Rotate (0, h, 0);
     FPSCamara.transform.Rotate (-v, 0 ,0 );
 
 
-    if ( Input.GetKey (KeyCode.W)) {
+        //movimiento del personaje
 
-      transform.Translate(0, 0 , 0.1f);
-   
-    } else {
-
-      if (Input.GetKey (KeyCode.S)){
-        transform.Translate (0, 0, -0.1f);
-      } else {
-
-        if (Input.GetKey (KeyCode.A)){
-        transform.Translate (-0.1f, 0, 0);
-      } else {
-        if (Input.GetKey (KeyCode.D)){
-        transform.Translate (0.1f, 0, 0);
-      } 
-      } 
-
-      }
-
-    }
-    
+        inputMov.x = Input.GetAxis("Horizontal");
+        inputMov.y = Input.GetAxis("Vertical");
 
 
 
 
    }
+
+
+   private void FixedUpdate()
+    {
+        //controladores
+        float vel = Input.GetKey(KeyCode.LeftShift) ? velCorre : velCamina;
+
+         rb.velocity =
+            transform.forward * vel * inputMov.y
+            + transform.right * vel * inputMov.x;
+
+    }
+
 }
